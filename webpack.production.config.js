8a6a16c336c95cecc66dee451a744cbcfbafe314
Vitaliy Vasilchenko <vet.vasilchenko@gmail.com>
2017-04-11 23:31:03 +0300

    made build

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,27 +2,49 @@ var webpack = require('webpack');
 var path = require('path');
 var loaders = require('./webpack.loaders');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: [
-        './src/index.jsx'
+        './src/index.js'
     ],
     output: {
-        path: path.join(__dirname, 'public'),
+        path: path.join( __dirname, 'public' ),
         filename: 'bundle.js'
     },
     resolve: {
-        extensions: ['', '.js', '.jsx']
+        extensions: ['', '.js', '.jsx', '.css'],
     },
     module: {
-        loaders: loaders.concat([{
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]')
-        }])
+        loaders: loaders.concat( [
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract( 'style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' )
+            },
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel',
+                include: [
+                    path.resolve( __dirname, './src' ),
+                ],
+            },
+        ])
     },
     plugins: [
-        new CopyWebpackPlugin([{
-            from: './index.html'
-        }]),
+        new CopyWebpackPlugin([{ from: './src/index.html', to: './index.html' }]),
+        new ExtractTextPlugin( 'styles.css' ),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin( {
+            sourceMap: true,
+            compress: {
+                'keep_fnames': true,
+                'screw_ie8': true,
+                'warnings': false,
+            },
+            mangle: {
+                'keep_fnames': true,
+                'screw_ie8': true,
+            },
+        } )
     ]
 };
