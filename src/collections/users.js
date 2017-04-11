ce1d0c967fc50aaa6e0fcef193f784378872cca4
Vitaliy Vasilchenko <vet.vasilchenko@gmail.com>
2017-04-11 19:51:19 +0300

    improve user model and collections

--- src/collections/users.js
+++ src/collections/users.js
@@ -1,15 +1,33 @@
-import { observable, action } from 'mobx';
+import { observable, action, computed, toJS } from 'mobx';
 
 import UserModel from '../models/user'
 
 class UsersCollection {
     @observable users = [];
 
+    get users () {
+        return toJS( this.users );
+    }
+
+    @computed get count () {
+        return this.users.length;
+    }
+
     @action( 'Remove user via Users' )
-    remove ( user ) {}
+    remove ( user ) {
+        this.users = this.users.filter( existedUser => existedUser.id !== user.id );
+    }
+
+    get model () {
+        return UserModel;
+    }
 
     @action( 'Add new user via Users' )
-    add ( name, company, phone ) {}
+    add ( { name, company, phone } ) {
+        return UserModel
+                    .create( name, company, phone )
+                    .then( result => { this.users.push( result ) } );
+    }
 
 }
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,63 @@
-import { observable } from 'mobx';
+import { observable, toJS } from 'mobx';
 
-export default class UserModel {
+import getGuid from '../utils/guid';
+
+class UserModel {
     @observable user = {};
-    @observable errors = {};
 
     create ( name, company, phone ) {
-        this.errors = {};
+        return new Promise( ( resolve, reject ) => {
+            const errors = this.validate( name, company, phone );
+            if ( Object.keys( errors ).length ) {
+                return reject( errors );
+            }
 
-        this.user = {
-            name,
-            company,
-            phone
-        };
+            this.user = {
+                id: getGuid(),
+                name,
+                company,
+                phone
+            };
 
-        return this.user;
+            resolve( this.user );
+        } );
     }
-
 
     validate ( name, company, phone ) {
+        const errors = {};
+
+        if ( !this.isValidName( name ) ) {
+            errors['name'] = 'Enter valid user name';
+        };
+
+        if ( !this.isValidCompany( company ) ) {
+            errors['company'] = 'Enter valid company name';
+        };
+
+        if ( !this.isValidPhone( phone ) ) {
+            errors['phone'] = 'Enter valid phone number';
+        };
+
+        return errors;
     }
 
-    isValid ( ) {}
+    isValidField ( fieldName, fieldValue ) {
+        const capName = fieldName.charAt( 0 ).toUpperCase() + fieldName.substring( 1 ).toLowerCase();
+        return this[ `isValid${capName}` ]( fieldValue );
+    }
+
+    isValidName ( name ) {
+        return Boolean( name && name.length > 3 );
+    }
+
+    isValidCompany ( company ) {
+        return Boolean( company && company.length > 3 );
+    }
+
+    isValidPhone ( phone ) {
+        return Boolean( !isNaN( Number( phone ) ) && String( phone ).length > 5 );
+    }
 
 }
+
+export default new UserModel;
